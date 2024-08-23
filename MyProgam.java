c7b288296e79785581eede5403e2a41116152bb4
240094 <ronit.dalal.094@k12.friscoisd.org>
2024-08-22 19:15:55 -0500

    first commit

---
+++ MyProgam.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+package TestRepo;
+public class MyProgam {
+
+    public static void main(String args[]) {
+        System.out.print("input your name");
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("Hello, %s/n", input.nextLine());
+    }
+}
