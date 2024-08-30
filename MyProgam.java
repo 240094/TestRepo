e496a1a4cc0972feffb2ec660c0c25a7a3f48827
240094 <ronit.dalal.094@k12.friscoisd.org>
2024-08-29 19:28:49 -0500

    learned about input

--- MyProgam.java
+++ MyProgam.java
@@ -1,23 +1,54 @@
 // Created by Ronit on Aug 22, 2024
-
 import java.util.Scanner;
-
 
 public class MyProgam {
 
 
-    int sumDouble(int a, int b) {
-        int c = a + b
-        if (a == b) {
-            return c * 2
-        }
-        return c;
+    public static void addNumbers(Scanner input) {
+
+        System.out.print("enter your number 1: ");
+        int num1 = input.nextInt();
+
+        System.out.print("enter your number 2: ");
+        int num2 = input.nextInt();
+
+        System.out.println(num1 + num2);
+    }
+
+
+    public static void subtractNumbers(Scanner input) {
+        System.out.print("enter your number 1: ");
+        int num1 = input.nextInt();
+
+        System.out.print("enter your number 2: ");
+        int num2 = input.nextInt();
+
+        System.out.println(num1 - num2);
     }
 
     public static void main(String args[]) {
-        System.out.print("input your name");
-        Scanner input = new Scanner(System.in);
 
-        System.out.printf("Hello, %s/n", input.nextLine());
+        while (true) {
+            
+            System.out.print("enter your input: ");
+            Scanner input = new Scanner(System.in);
+            String string = input.nextLine();
+
+            if (string.equals("add")) {
+                addNumbers(input);
+                continue;
+            } 
+
+            else if (string.equals("subtract")) {
+                subtractNumbers(input);
+                continue;
+            }
+
+            else if (string.equals("exit")) {
+                break;
+            }
+
+            System.out.println(string);
+        }
     }
 }
